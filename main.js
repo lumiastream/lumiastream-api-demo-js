af39cc411a8b0cddf8deef8535941bbd529b4c9d
AhadCove <ahadcove@gmail.com>
2020-02-16 23:34:50 -0500

    Simpler demo

--- main.js
+++ main.js
@@ -2,27 +2,18 @@ const axios = require('axios');
 
 const token = 'paste-token-here'; // Paste token from Lumia Stream (Settings > Advanced > API > Rest API > Show Token)
 
+// Get Possible Types and Values
 axios.get(`http://localhost:39231/api/retrieve?token=${token}`)
 	.then((res) => {
-		console.log('Api option res: ', res.data.data);
-
-		// Retrieve Chat Colors from the returned array
-		const chatColors = res.data.data.find((command) => command.type === 'chat-color');
-
-		console.log('chatColors: ', chatColors);
-		const firstColor = chatColors.values[0];
-		console.log('firstColor: ', firstColor);
-		sendCommand({ type: chatColors.type, value: firstColor });
+		console.log('Api option res: ', res.data);
 	}).catch((err) => {
 		console.log('err: ', err);
 	});
 
-
-const sendCommand = ({ type, value }) => {
-	axios.post(`http://localhost:39231/api/send?token=${token}`, { type, value })
-		.then((res) => {
-			console.log('res: ', res.data);
-		}).catch((err) => {
-			console.log('err: ', err);
-		});
-}
+// Trigger Chat Color Blue
+axios.post(`http://localhost:39231/api/send?token=${token}`, { type: 'chat-color', value: 'blue' })
+	.then((res) => {
+		console.log('res: ', res.data);
+	}).catch((err) => {
+		console.log('err: ', err);
+	});
